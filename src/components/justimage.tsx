d215ac2944626a5c1211b510947b73a8a00390f6
sanjarjelet13 <126490244+sanjarjelet13@users.noreply.github.com>
2025-01-17 22:14:31 -0500

    removed placeholder images, updated navbar with info

--- src/components/justimage.tsx
+++ src/components/justimage.tsx
@@ -9,75 +9,75 @@ const JustImage: React.FC = () => {
     console.log(imageMap);
   }, []);
 
-  const showTooltip = (tooltipId: string) => {
-    const tooltip = document.getElementById(tooltipId);
+  // const showTooltip = (tooltipId: string) => {
+  //   const tooltip = document.getElementById(tooltipId);
 
-    if (tooltip) {
-      tooltip.style.display = "block";
-    }
-  };
+  //   if (tooltip) {
+  //     tooltip.style.display = "block";
+  //   }
+  // };
 
-  const hideTooltip = (tooltipId: string) => {
-    const tooltip = document.getElementById(tooltipId);
+  // const hideTooltip = (tooltipId: string) => {
+  //   const tooltip = document.getElementById(tooltipId);
 
-    if (tooltip) {
-      tooltip.style.display = "none";
-    }
-  };
+  //   if (tooltip) {
+  //     tooltip.style.display = "none";
+  //   }
+  // };
 
-  const handleMouseOver = (
-    e: React.MouseEvent<HTMLAreaElement>,
-    tooltipId: string,
-  ) => {
-    console.log("mouse is entered");
+  // const handleMouseOver = (
+  //   e: React.MouseEvent<HTMLAreaElement>,
+  //   tooltipId: string,
+  // ) => {
+  //   console.log("mouse is entered");
 
-    const tooltip = document.getElementById(tooltipId);
-    if (!tooltip) return;
-    const area = e.target as HTMLAreaElement;
-    const coords = area.getAttribute("coords")?.split(",").map(Number);
-    // get size of rectangle from coordinates
-    if (!coords) return;
+  //   const tooltip = document.getElementById(tooltipId);
+  //   if (!tooltip) return;
+  //   const area = e.target as HTMLAreaElement;
+  //   const coords = area.getAttribute("coords")?.split(",").map(Number);
+  //   // get size of rectangle from coordinates
+  //   if (!coords) return;
 
-    // Extract width and height from coords
-    const [x1, y1, x2, y2] = coords;
-    const width = x2 - x1;
-    const height = y2 - y1;
+  //   // Extract width and height from coords
+  //   const [x1, y1, x2, y2] = coords;
+  //   const width = x2 - x1;
+  //   const height = y2 - y1;
 
-    showTooltip(tooltipId);
+  //   showTooltip(tooltipId);
 
-    tooltip!.style.left = `${width}px`;
-    tooltip!.style.top = `${height}px`;
-  };
+  //   tooltip!.style.left = `${width}px`;
+  //   tooltip!.style.top = `${height}px`;
+  // };
 
-  const handleMouseOut = (
-    e: React.MouseEvent<HTMLAreaElement>,
-    tooltipId: string,
-  ) => {
-    console.log("mouse is out");
-    hideTooltip(tooltipId);
-  };
+  // const handleMouseOut = (
+  //   e: React.MouseEvent<HTMLAreaElement>,
+  //   tooltipId: string,
+  // ) => {
+  //   console.log("mouse is out");
+  //   hideTooltip(tooltipId);
+  // };
 
-  const handleMouseMove = (
-    e: React.MouseEvent<HTMLAreaElement>,
-    tooltipId: string,
-  ) => {
-    console.log("mouse mvoed");
-    const tooltip = document.getElementById(tooltipId);
-    if (!tooltip) return;
+  // const handleMouseMove = (
+  //   e: React.MouseEvent<HTMLAreaElement>,
+  //   tooltipId: string,
+  // ) => {
+  //   console.log("mouse mvoed");
+  //   const tooltip = document.getElementById(tooltipId);
+  //   if (!tooltip) return;
 
-    const parent = document.getElementById("map-parent");
-    if (!parent) return;
+  //   const parent = document.getElementById("map-parent");
+  //   if (!parent) return;
 
-    const parentRect = parent.getBoundingClientRect();
+  //   const parentRect = parent.getBoundingClientRect();
 
-    const x = e.clientX - parentRect.left;
-    const y = e.clientY - parentRect.top;
-    if (tooltip.style.display !== "block") {
-      showTooltip(tooltipId);
-    }
-    document.getElementById(tooltipId)!.style.left = `${x + 10}px`;
-    document.getElementById(tooltipId)!.style.top = `${y + 16}px`;
-  };
+  //   const x = e.clientX - parentRect.left;
+  //   const y = e.clientY - parentRect.top;
+  //   if (tooltip.style.display !== "block") {
+  //     showTooltip(tooltipId);
+  //   }
+  //   document.getElementById(tooltipId)!.style.left = `${x + 10}px`;
+  //   document.getElementById(tooltipId)!.style.top = `${y + 16}px`;
+  // };
 
   return (
     // <div id="map-parent" className="relative w-[1000px] h-[1000px]">
@@ -85,7 +85,7 @@ const JustImage: React.FC = () => {
       <div id="map-parent" className="relative w-full aspect-square">
         <map name="imagemap">
           {/* Tooltip Divs - Initially Hidden */}
-          <div
+          {/* <div
             id="tooltip1"
             className={`absolute bg-white border border-gray-500 p-2 shadow-md text-sm opacity-100 hidden  z-10`}
           >
@@ -178,9 +178,9 @@ const JustImage: React.FC = () => {
             onMouseOver={(e) => handleMouseOver(e, "tooltip6")}
             onMouseOut={(e) => handleMouseOut(e, "tooltip6")}
             onMouseMove={(e) => handleMouseMove(e, "tooltip6")}
-          />
+          /> */}
         </map>
-        <img useMap="#imagemap" src="/20by20gridwbg.png" alt="Test Image" />
+        <img useMap="#imagemap" src="/20by20initialgrid.png" alt="Test Image" />
       </div>
     </div>
   );
