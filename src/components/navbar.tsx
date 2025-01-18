52c3c4c3fd82b078729a54b6fcd17c590ccbf4bd
sanjarjelet13 <126490244+sanjarjelet13@users.noreply.github.com>
2025-01-17 22:17:10 -0500

    testing

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -10,7 +10,7 @@ const Navbar: React.FC = () => {
     <nav className="w-full h-auto py-5 px-4">
       <div className="container mx-auto max-w-[1000px] flex items-center justify-between mb-2">
         <h1 className="text-2xl font-bold">Vegan One Million</h1>
-        <p className="font-bold">2500 Slots Left</p>
+        <p className="font-bold">2500 Slots Lef</p>
       </div>
       <div className="container mx-auto max-w-[1000px] flex items-center justify-center mb-4">
         <ul className="container flex gap-4 w-full items-center justify-center h-full px-4">
